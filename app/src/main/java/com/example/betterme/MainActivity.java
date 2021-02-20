63148cd96cc9bfd18c13955940800e66cfef7439
kaelanewman <43575644+kaelanewman@users.noreply.github.com>
2021-02-20 16:24:05 -0600

    Added Bottom Navigation Bar and Fragment Navigation

--- app/src/main/java/com/example/betterme/MainActivity.java
+++ app/src/main/java/com/example/betterme/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.betterme;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_history:
+                            selectedFragment = new HistoryFragment();
+                            break;
+                    }
+
+                    //replace the fragment view
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    //true indicates we want to select the menu item
+                    return true;
+                }
+            };
 }
